c76ebf9afc3392744baeed6778dc114fbffa9f0d
chau-nm <chaunm.146.2001@gmail.com>
2026-01-11 20:39:43 +0700

    feat: update experience

--- src/feature/main/MyProject/data.tsx
+++ src/feature/main/MyProject/data.tsx
@@ -1,4 +1,7 @@
 import KnowledgePortal from "assets/images/knowledge-portal.png";
+import PosRetail from "assets/images/pos-retail.png";
+import IllustrationCoder from "assets/images/illustration_coder.png";
+import SocialTool from "assets/images/social-tool.png";
 import { TFunction } from "i18next";
 import { ReactNode } from "react";
 
@@ -15,6 +18,40 @@ export const projectData = (t: TFunction): ProjectType[] => [
 		title: t("myProject.knowledgePortal.title"),
 		description: <p>{t("myProject.knowledgePortal.description")}</p>,
 		technologies: ["React", "Yii2", "MySQL", "Docker", "AWS"],
+	},
+	{
+		image: PosRetail,
+		title: t("myProject.posRetail.title"),
+		description: <p>{t("myProject.posRetail.description")}</p>,
+		technologies: [
+			"Flutter",
+			"Cloudflare",
+			"Symfony",
+			"API Platform",
+			"PostgreSQL",
+			"Caddy",
+			"Docker",
+			"GitHub Actions",
+			"Sentry",
+			"OpenSearch",
+			"DigitalOcean App Platform",
+		],
+	},
+	{
+		image: SocialTool,
+		title: t("myProject.socialTool.title"),
+		description: <p>{t("myProject.socialTool.description")}</p>,
+		technologies: [
+			"Vue.js",
+			"Cloudflare",
+			"Yii2",
+			"MySQL",
+			"Docker",
+			"GitHub Actions",
+			"Sentry",
+			"OpenSearch",
+			"DigitalOcean App Platform",
+		],
 	},
 ];
 
